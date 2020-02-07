5e1c25d2c15ab79f69c5a5d06302770decf80ea1
vamsiteja142 <43532941+vamsiteja142@users.noreply.github.com>
2020-02-07 23:33:52 +0530

    Add files via upload

---
+++ admin.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Products</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">      
+    </head>
+    <body>
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="index.php">Welcome</a>
+            </div>
+            <div id="navbar" class="navbar-collapse collapse">
+                <ul class="nav navbar-nav">
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="admin.php"> Admin panel</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <script src="http://code.jquery.com/jquery-latest.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script> $(function(){ $('.nav-tabs a:first').tab('show'); }); </script> </body>
+</body>
+
+</html> 
---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <title>Products</title>
+        <link rel="stylesheet" href="bootstrap.min.css"> 
+        
+    </head>
+<body>
+    
+</body>
+
+</html> 
